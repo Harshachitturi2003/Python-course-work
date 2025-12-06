f24797f2291e75deaaa6ee9e3ea5b93dff131824
Harshachitturi2003 <harshachitturi2003@gmail.com>
2025-12-06 19:05:19 +0530

    lambda fun

---
+++ class works/lambda fun.py
@@ -0,0 +1,10 @@
+def wish(name):
+    return f'Welcome to Python {name}'
+print(wish("Santosh"))
+print(wish("Ramya"))
+print(wish("Chandana"))
+    
+lwish=lambda name : f'Welcome to python {name}!'
+print(lwish("Santosh"))
+print(lwish("Ramya"))
+print(lwish("Chandana"))
